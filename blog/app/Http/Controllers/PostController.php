605defbae359f963c680163f8ce8eb51b5d58c14
AbdEl-Hamid Muhammad <dev-hamid@outlook.com>
2017-09-03 03:43:06 +0200

    Add files via upload

---
+++ blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Like;
+use App\Tag;
+use App\User;
+use Auth;
+use Gate;
+
+class PostController extends Controller {
+    public function getIndex() {
+      $posts = Post::orderBy('created_at','desc')->paginate(3);
+      return View('blog.index', ['posts' => $posts]);
+    }
+
+    public function getAdminIndex() {
+        $posts = Post::orderBy('title','asc')->get();
+        return View('admin.index', ['posts' => $posts]);
+    }
+
+    public function getPost($id) {
+        $post = Post::where('id', $id)->with('likes')->first();
+        return view('blog.post', ['post' => $post]);
+    }
+
+    public function getLikePost($id) {
+        $post = Post::find($id);
+        $like = new Like();
+        $post->likes()->save($like);
+        return redirect()->back();
+    }
+
+    public function getAdminCreate() {
+        $tags = Tag::all();
+        return view('admin.create', ['tags' => $tags]);
+    }
+
+    public function getAdminEdit($id) {
+        $tags = Tag::all();
+        $post = Post::find($id);
+        return view('admin.edit', ['post' => $post, 'postId' => $id], ['tags' => $tags]);
+    }
+
+    public function postAdminCreate(Request $request) {
+        $this->validate($request , [
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        $post = new Post([
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
+          ]);
+        $user->posts()->save($post);
+        $post->tags()->attach(
+          $request->input('tags')=== null?[]:$request->input('tags'));
+        return redirect()
+        ->route('admin.index')
+        ->with('info', 'post created, Title is: '.$request->input('title'));
+    }
+
+    public function postAdminUpdate(Request $request) {
+        $this->validate($request , [
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        $post = Post::find($request->input('id'));
+        if (Gate::denies('manipulate-post', $post)) {
+            return redirect()->back();
+        }
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+        $post->tags()->sync(
+          $request->input('tags')=== null ?[]:$request->input('tags'));
+        return redirect()
+        ->route('admin.index')
+        ->with('info', 'post edited, new Title is: '.$request->input('title'));
+    }
+
+    public function postAdminDelete($id) {
+        $post = Post::find($id);
+        if (Gate::denies('maipulate-post', $post)) {
+            return redirect()->back();
+        }
+        $post->likes()->delete();
+        $post->tags()->detach();
+        $post->delete();
+        return redirect()
+        ->route('admin.index')
+        ->with('info', 'post deleted');
+    }
+}
