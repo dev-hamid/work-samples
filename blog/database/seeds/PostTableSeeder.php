7d3e46319f285eac376a60af9582f17210822f59
AbdEl-Hamid Muhammad <dev-hamid@outlook.com>
2017-09-03 03:44:05 +0200

    Add files via upload

---
+++ blog/database/seeds/PostTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        // $post = new \App\Post();
+        // $post->title = 'new post 1';
+        // $post->content ='some dummy content';
+        // $post->save();
+        //
+        // $post = new \App\Post();
+        // $post->title = 'Test post 2';
+        // $post->content ='some other very dummy content';
+        // $post->save();
+
+    }
+}
